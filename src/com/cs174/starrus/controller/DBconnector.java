44da29b52663e881f1221f0ff42498d8bcd8db0b
Peng Wang <pengwang01@umail.ucsb.edu>
2013-02-24 20:28:53 -0800

    Add db connector
    Now it's able to connect to db and ready to do some queries.

---
+++ src/com/cs174/starrus/controller/DBconnector.java
@@ -0,0 +1,37 @@
+package com.cs174.starrus.controller;
+import java.sql.*;
+
+import oracle.jdbc.driver.*;
+
+public class DBconnector {
+	private Connection conn = null;
+	private String strConn = "jdbc:oracle:thin:@uml.cs.ucsb.edu:1521:xe"; 
+	private String strUsername = "cs174a_pengwang01";
+	private String strPassword = "5891775";
+	
+	
+	public void DBconnector(){
+	}
+	
+	public void connect() throws SQLException{
+			DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+			conn = DriverManager.getConnection(strConn,strUsername,strPassword);
+	}
+	
+	public void disconnect() throws SQLException{
+		conn.close();
+	}
+
+	public void test() throws SQLException { 
+		// Create a Statement
+		Statement stmt = conn.createStatement(); // Specify the SQL Query to run
+		ResultSet rs = stmt.executeQuery ("SELECT username FROM customer"); // Iterate through the result and print the data
+		System.out.println("result:");
+		//System.out.println(rs.toString());
+		while(rs.next()){
+			System.out.println(rs.getString(1));
+		}
+		// don't miss this
+		rs.close();
+	}
+}
--- src/com/cs174/starrus/controller/RunApp.java
+++ src/com/cs174/starrus/controller/RunApp.java
@@ -1,13 +1,27 @@
 package com.cs174.starrus.controller;
 
+import java.sql.SQLException;
+
 import com.cs174.starrus.view.Window;
 
 public class RunApp {
 	public static void main(String[] args){
+		
+		DBconnector db = new DBconnector();
+		try {
+			db.connect();
+			db.test();
+			db.disconnect();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		Window app = new Window();
-		app.launchLoginWindow();
+		//app.launchLoginWindow();
 		
 		Window app2 = new Window();
-		app2.launchMainWindow();
+		//app2.launchMainWindow();
 	}
 }
