82e6ae520e70e72db7bfda73c232fefadf02b7cc
Bryan <Yu.Bryan.J@gmail.com>
2013-03-09 00:40:52 -0800

    Money_Trans table is now updated whenever a withdrawal is done. Will work on deposit side next. Should be same code.

--- src/com/cs174/starrus/controller/WithdrawSubmitController.java
+++ src/com/cs174/starrus/controller/WithdrawSubmitController.java
@@ -9,7 +9,12 @@ import com.cs174.starrus.view.CustomerView;
 import com.cs174.starrus.view.IView;
 import com.cs174.starrus.view.WithdrawView;
 
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class WithdrawSubmitController implements IController{
+	private boolean DEBUG = true;
 	private Connection conn = null;
 	private Customer c = Customer.getCustomer();
 	private WithdrawView wdView = WithdrawView.getView();
@@ -29,25 +34,45 @@ public class WithdrawSubmitController implements IController{
 			float balance = 0;
 			stmt = conn.createStatement();
 			
-			// checking for if enough money to withdraw
-			if(c.getBalance() - Float.parseFloat(wdView.getTxtWithdraw().getText()) < 0){
+			if(c.getBalance() - Float.parseFloat(wdView.getTxtWithdraw().getText()) < 0)
 				wdView.getLblWarning().setText("not enough money");
-				wdView.getTxtWithdraw().setText(null);
-			}
-			else{
-				balance = c.getBalance() - Float.parseFloat(wdView.getTxtWithdraw().getText());  
-				c.setBalance(balance);
+			
+			balance = c.getBalance() - Float.parseFloat(wdView.getTxtWithdraw().getText());  
+			c.setBalance(balance);
 				
-				// execute query for withdrawing money from balance
-				stmt.executeQuery("UPDATE Customer set balance = " + balance + 
-									"WHERE username = '" + c.getUsername() + "'");
-				cView.getBalancefiled().setText(Float.toString(balance));
-				wdView.getTxtWithdraw().setText(null);
-				wdView.setVisible(false);
-				
-				// execute query for creating money transaction row in the transaction table
-				// add code here
+			// Insert into transacation table
+			DateFormat format 	= new SimpleDateFormat("dd-MMM-yy");
+			Date today 			= new Date();
+			String dateString	= format.format(today);
+
+			// FOR DEBUGGING PURPOSES
+			if( DEBUG == true ){
+				System.out.println( "Username: "+ c.getUsername()   +" \n" +
+									"Date: "    + dateString        +" \n" +
+									"Balance: " + c.getBalance()
+									);
+				System.out.println("INSERT INTO MONEY_TRANS (TDATE,TUSERNAME,TTYPE,AMOUNT) VALUES ("
+									+ "'"	+ dateString        + "'"	+ ","
+									+ "'" 	+ c.getUsername()   + "'"	+ ","
+									+ 2                 + ","
+									+ c.getBalance()    + ")"
+									);
 			}
+
+			String mtStr		= "INSERT INTO MONEY_TRANS (TDATE,TUSERNAME,TTYPE,AMOUNT) VALUES (" 
+								+ "'"	+ dateString        + "'"	+ ","
+								+ "'" 	+ c.getUsername()   + "'"	+ ","
+								+ 2					+ "," 
+								+ c.getBalance()	+ ")";
+									
+			stmt.executeQuery(mtStr);
+					
+			stmt.executeQuery("UPDATE Customer set balance = " 	+ balance + 
+								"WHERE username = '" 			+ c.getUsername() + "'");
+
+			cView.getBalancefiled().setText(Float.toString(balance));
+			wdView.getTxtWithdraw().setText(null);
+			wdView.setVisible(false);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
