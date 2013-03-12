ec6bbbc050e37ecdb3c7566ee4bdb09c8384d40b
Bryan <Yu.Bryan.J@gmail.com>
2013-03-12 11:31:00 -0700

    View

---
+++ src/com/cs174/starrus/model/SysDate.java
@@ -0,0 +1,38 @@
+package com.cs174.starrus.model;
+
+import java.lang.String;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SysDate{
+	private static SysDate	SysDate = null;
+	private String 			dateStr = "";
+
+	public SysDate(String date){
+		this.dateStr=date;
+	}
+
+	public String getDateStr(){
+		return this.dateStr;
+	}
+
+	public void setDateStr(String date){
+		this.dateStr=date;
+	}
+
+	public static SysDate getSysDate(){
+		if(SysDate == null){
+			DateFormat 	format	= new SimpleDateFormat("dd-MMM-yy");
+			Date 		today	= new Date();
+			String 		dateStr	= format.format(today);
+			
+			SysDate = new SysDate(dateStr);
+		}
+		return SysDate;
+	}
+	public static void setSysDate(SysDate date){
+		SysDate	= date;
+	}
+
+}
